3a1daae6c93665e6ca67b8d2cade2978035ae4ee
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-13 15:21:54 +0530

    Initial Commit

---
+++ transpose/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  transpose
+//
+//  Created by Hibrise on 13/08/14.
+//  Copyright (c) 2014 Hibrise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

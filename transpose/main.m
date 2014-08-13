92362680f0c3cd95ea7f6d8378b66cb8ef0134de
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-13 16:25:21 +0530

    transpose
    transpose

--- transpose/main.m
+++ transpose/main.m
@@ -12,10 +12,91 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int j,i;
+        int a[3][3]={ {9,2,3},{3,6,4},{3,4,7} };
         
+        
+       int  b[3][3]=
+        { {0,3,1},{2,0,2},{3,2,0}};
+        int c[3][3];
+        
+//        
+//        for(int i=0;i<3;i++)
+//        {
+//            
+//            for(int j=0;j<3;j++)
+//            {
+//                printf("enter the number a\n");
+//                scanf("%d",&a[i][j]);
+//                
+//            }
+//        }
+        for(int i=0;i<3;i++)
+        {
+            
+            for(int j=0;j<3;j++)
+            {
+               a[0][0]=0;
+                a[1][1]=0;
+               a[2][2]=0;
+                NSLog(@" the number of a is %d",a[i][j]);
+                
+                
+            }
+        }
+//        for(int i=0;i<3;i++)
+//        {
+//            
+//            for(int j=0;j<3;j++)
+//            {
+//                NSLog(@"enter the number b");
+//               // scanf("%d",&b[i][j]);
+//                
+//            }
+//        }
+        for(int i=0;i<3;i++)
+        {
+            
+            for(int j=0;j<3;j++)
+            {
+                b[0][0]=0;
+                b[1][1]=0;
+               b[2][2]=0;
+                NSLog(@" the number of b is %d",b[i][j]);
+                
+                
+            }
+        }
+        for(int i=0;i<3;i++)
+        {
+            
+            for(int j=0;j<3;j++)
+            {
+                c[i][j]=a[i][j]+b[i][j];                }
+        }
+        for(int i=0;i<3;i++)
+        {
+            
+            for(int j=0;j<3;j++)
+            {
+//                c[0][0]=0;
+//                c[1][1]=0;
+//                c[2][2]=0;
+                NSLog(@" the answer is %d",c[i][j]);
+                
+                
+            }
+        }
+        
+        
+        
+        
+    
+
+    
         // insert code here...
         NSLog(@"Hello, World!");
-        
+    
     }
     return 0;
 }
